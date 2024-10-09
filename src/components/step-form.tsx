7669ff98e18ec113b152ac6836f01f28dcb0a2ff
lamtanphat2202 <tphatt22@gmail.com>
2024-10-09 23:52:28 +0700

    update page donate

--- src/components/step-form.tsx
+++ src/components/step-form.tsx
@@ -1,26 +1,18 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
-
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { Check, ChevronsUpDown } from "lucide-react";
-
-import { cn } from "@/lib/utils";
-import {
-    Command,
-    CommandEmpty,
-    CommandGroup,
-    CommandInput,
-    CommandItem,
-    CommandList,
-} from "@/components/ui/command";
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
 
 const frameworks = [
     {
@@ -49,7 +41,6 @@ export default function StepForm() {
     const [selectAmount, setSelectAmount] = useState(0);
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState("");
-
     const amounts = [10, 50, 100];
 
     const handleSelectAmount = (index: number) => {
@@ -64,7 +55,7 @@ export default function StepForm() {
         setStep((prevStep) => (prevStep <= 0 ? 0 : prevStep - 1));
     };
     return (
-        <div>
+        <div className="">
             {/* State form 1 */}
             {step === 0 && (
                 <Card>
@@ -72,12 +63,15 @@ export default function StepForm() {
                         <div className=" text-center font-semibold">
                             Choose your amount
                         </div>
-                        <img
-                            className="w-[30px] h-[30px] hover:opacity-40 absolute top-[16px] right-[5%] cursor-pointer"
-                            src="/images/arrow-right.svg"
-                            alt="Right arrow"
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 448 512"
+                            className="w-[23px] h-[23px] hover:opacity-40 absolute top-[21px] right-[5%] cursor-pointer"
                             onClick={handleNextForm}
-                        />
+                        >
+                            <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+                        </svg>
                     </div>
                     <CardContent className="space-y-2">
                         <div>
@@ -110,67 +104,9 @@ export default function StepForm() {
 
                         <div className="!mt-[15px]">
                             <p className="text-[#2dd4aa] text-[0.7rem] mb-[10px]">
-                                Where would you like your donation to go?
+                                Do you have any description ?
                             </p>
-                            {/* Drop Down choose branch want to donate */}
-                            <Popover open={open} onOpenChange={setOpen}>
-                                <PopoverTrigger asChild>
-                                    <Button
-                                        variant="outline"
-                                        role="combobox"
-                                        aria-expanded={open}
-                                        className="justify-between w-[100%]"
-                                    >
-                                        {value
-                                            ? frameworks.find(
-                                                  (framework) =>
-                                                      framework.value === value
-                                              )?.label
-                                            : "Select your branch..."}
-                                        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                                    </Button>
-                                </PopoverTrigger>
-                                <PopoverContent className="w-[350px] p-0">
-                                    <Command>
-                                        <CommandInput placeholder="Search framework..." />
-                                        <CommandList>
-                                            <CommandEmpty>
-                                                No framework found.
-                                            </CommandEmpty>
-                                            <CommandGroup>
-                                                {frameworks.map((framework) => (
-                                                    <CommandItem
-                                                        key={framework.value}
-                                                        value={framework.value}
-                                                        onSelect={(
-                                                            currentValue
-                                                        ) => {
-                                                            setValue(
-                                                                currentValue ===
-                                                                    value
-                                                                    ? ""
-                                                                    : currentValue
-                                                            );
-                                                            setOpen(false);
-                                                        }}
-                                                    >
-                                                        <Check
-                                                            className={cn(
-                                                                "mr-2 h-4 w-2",
-                                                                value ===
-                                                                    framework.value
-                                                                    ? "opacity-100"
-                                                                    : "opacity-0"
-                                                            )}
-                                                        />
-                                                        {framework.label}
-                                                    </CommandItem>
-                                                ))}
-                                            </CommandGroup>
-                                        </CommandList>
-                                    </Command>
-                                </PopoverContent>
-                            </Popover>
+                            <Input id="name" placeholder="Your description" />
                         </div>
                     </CardContent>
                     <CardFooter>
@@ -179,101 +115,85 @@ export default function StepForm() {
                             className="hover:opacity-75"
                         >
                             Next Step
-                            <img
-                                src="/images/gravity-arrow-right.svg"
-                                alt="Arrow Icon"
-                                className="ml-2 h-4 w-4"
-                            />
                         </Button>
                     </CardFooter>
                 </Card>
             )}
-
-            {/* State form 2 */}
+            State form 2
             {step === 1 && (
                 <Card>
                     <div className="relative p-[20px]">
-                        <img
-                            className="w-[30px] h-[30px] hover:opacity-40 absolute top-[16px] left-[5%] cursor-pointer"
-                            src="/images/arrow-left.svg"
-                            alt="Right arrow"
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 448 512"
+                            className="w-[23px] h-[23px] hover:opacity-40 absolute top-[21px] left-[5%] cursor-pointer"
                             onClick={handlePreForm}
-                        />
-                        <div className=" text-center font-semibold">
-                            Your Information
-                        </div>
-                        <img
-                            className="w-[30px] h-[30px] hover:opacity-40 absolute top-[16px] right-[5%] cursor-pointer"
-                            src="/images/arrow-right.svg"
-                            alt="Right arrow"
-                            onClick={handleNextForm}
-                        />
-                    </div>
-                    <CardContent className="space-y-2">
-                        <div className="!mt-[15px] relative">
-                            <Label htmlFor="first-name">First Name</Label>
-                            <Input id="first-name" />
-                            <img
-                                src="/images/ic-user.svg"
-                                className="absolute top-[29px] right-0 flex items-center pr-3 opacity-60"
-                            />
-                        </div>
-                        <div className="!mt-[15px] relative">
-                            <Label htmlFor="last-name">Last Name</Label>
-                            <Input id="last-name" />
-                        </div>
-                        <div className="!mt-[15px] relative">
-                            <Label htmlFor="email">Email</Label>
-                            <Input id="email" />
-                            <img
-                                src="/images/ic-email.svg"
-                                className="absolute top-[29px] right-0 flex items-center pr-3 opacity-60"
-                            />
-                        </div>
-                    </CardContent>
-                    <CardFooter>
-                        <Button
-                            onClick={handleNextForm}
-                            className="hover:opacity-75"
                         >
-                            Next Step
-                            <img
-                                src="/images/gravity-arrow-right.svg"
-                                alt="Arrow Icon"
-                                className="ml-2 h-4 w-4"
-                            />
-                        </Button>
-                    </CardFooter>
-                </Card>
-            )}
-
-            {/* State form 3 */}
-            {step === 2 && (
-                <Card>
-                    <div className="relative p-[20px]">
-                        <img
-                            className="w-[30px] h-[30px] hover:opacity-40 absolute top-[16px] left-[5%] cursor-pointer"
-                            src="/images/arrow-left.svg"
-                            alt="Right arrow"
-                            onClick={handlePreForm}
-                        />
+                            <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+                        </svg>
                         <div className=" text-center font-semibold">
-                            Payment
+                            Choose your payment method
                         </div>
                     </div>
-                    <CardContent className="space-y-2">
-                        <div className="space-y-1">
-                            <Label htmlFor="current">Current password</Label>
-                            <Input id="current" type="password" />
-                        </div>
-                        <div className="space-y-1">
-                            <Label htmlFor="new">New password</Label>
-                            <Input id="new" type="password" />
-                        </div>
-                    </CardContent>
-                    <CardFooter>
-                        <Button>Finish Donation</Button>
-                    </CardFooter>
+                    <Tabs defaultValue="account" className="w-full mt-10">
+                        <TabsList className="grid w-full grid-cols-2">
+                            <TabsTrigger value="account">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 640 512"
+                                    className="w-[23px] h-[23px]"
+                                >
+                                    <path d="M96 96l0 224c0 35.3 28.7 64 64 64l416 0c35.3 0 64-28.7 64-64l0-224c0-35.3-28.7-64-64-64L160 32c-35.3 0-64 28.7-64 64zm64 160c35.3 0 64 28.7 64 64l-64 0 0-64zM224 96c0 35.3-28.7 64-64 64l0-64 64 0zM576 256l0 64-64 0c0-35.3 28.7-64 64-64zM512 96l64 0 0 64c-35.3 0-64-28.7-64-64zM288 208a80 80 0 1 1 160 0 80 80 0 1 1 -160 0zM48 120c0-13.3-10.7-24-24-24S0 106.7 0 120L0 360c0 66.3 53.7 120 120 120l400 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-400 0c-39.8 0-72-32.2-72-72l0-240z" />
+                                </svg>
+                            </TabsTrigger>
+                            <TabsTrigger value="password">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 576 512"
+                                    className="w-[23px] h-[23px]"
+                                >
+                                    <path d="M64 64C28.7 64 0 92.7 0 128L0 384c0 35.3 28.7 64 64 64l448 0c35.3 0 64-28.7 64-64l0-256c0-35.3-28.7-64-64-64L64 64zm48 160l160 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-160 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zM96 336c0-8.8 7.2-16 16-16l352 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-352 0c-8.8 0-16-7.2-16-16zM376 160l80 0c13.3 0 24 10.7 24 24l0 48c0 13.3-10.7 24-24 24l-80 0c-13.3 0-24-10.7-24-24l0-48c0-13.3 10.7-24 24-24z" />
+                                </svg>
+                            </TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="account">
+                            <Card className="!border-0">
+                                <CardHeader>
+                                    <CardTitle>Cash method</CardTitle>
+                                    <CardDescription className="!mt-[10px]">
+                                        If you donate in cash, please click the
+                                        button below for more details.
+                                    </CardDescription>
+                                </CardHeader>
+                                <CardFooter>
+                                    <Button>Cash guide </Button>
+                                </CardFooter>
+                            </Card>
+                        </TabsContent>
+                        <TabsContent value="password">
+                            <Card className="!border-0">
+                                <CardHeader>
+                                    <CardTitle>Withdraw method</CardTitle>
+                                </CardHeader>
+
+                                <CardContent className="space-y-2">
+                                    <div className="space-y-1">
+                                        <Label htmlFor="current">
+                                            Enter your cardId
+                                        </Label>
+                                        <Input
+                                            id="current"
+                                            type="password"
+                                            placeholder="cardId"
+                                        />
+                                    </div>
+                                </CardContent>
+                                <CardFooter>
+                                    <Button>Finish Payment</Button>
+                                </CardFooter>
+                            </Card>
+                        </TabsContent>
+                    </Tabs>
                 </Card>
             )}
         </div>
