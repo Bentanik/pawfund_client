0ed88d7d1c1c88d8ecda5b2fc3619c837c62099e
lamtanphat2202 <tphatt22@gmail.com>
2024-10-17 05:26:17 +0700

    commit complete get profile user

---
+++ src/services/event/typings.d.ts
@@ -0,0 +1,53 @@
+declare namespace API {
+  enum AuthRole {
+    USER = "user",
+    STAFF = "staff",
+    ADMIN = "admin",
+  }
+
+  type TAuthResponse = {
+    token: TAuthToken;
+    authProfile: TAuthProfile;
+  };
+
+  type TAuthVerifyEmail = {
+    email: string;
+  };
+
+  type TAuthForgotPasswordEmail = {
+    email: string;
+  };
+
+  type TAuthForgotPasswordOtp = {
+    email: string;
+    otp: string;
+  };
+
+  type TAuthForgotPasswordChange = {
+    email: string;
+    otp: string;
+    password: string;
+  };
+
+  type TAuthToken = {
+    accessToken: string;
+    tokenType: string;
+  };
+
+  type TAuthLoginGoogle = {
+    accessTokenGoogle: string;
+  };
+
+  type TAuthProfile = {
+    userId: string;
+    firstName: string;
+    lastName: string;
+    avatarLink: string;
+    role: AuthRole;
+  };
+
+  type TAuthForgotPassword = {
+    email: string;
+    otp: string;
+  };
+}
