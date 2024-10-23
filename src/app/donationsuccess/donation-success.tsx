4d253decff92b607897e3dac79f823a1c7593985
lamtanphat2202 <tphatt22@gmail.com>
2024-10-23 11:05:45 +0700

    design response donation page

--- src/app/donationsuccess/donation-success.tsx
+++ src/app/donationsuccess/donation-success.tsx
@@ -9,16 +9,19 @@ export const metadata: Metadata = {
 export default function DonationSuccess({ params }: any) {
     return (
         <div className="w-full h-screen object-cover overflow-hidden relative">
-            <img
-                src="/images/bgDonation.jpg"
-                alt="bg"
-                className="w-full h-full object-cover"
-            />
+            <div className="relative w-full h-full">
+                <img
+                    src="/images/bgDonation.jpg"
+                    alt="bg"
+                    className="w-full h-full object-fill"
+                />
+                <div className="absolute inset-0 bg-[#00000023] opacity-50"></div>
+            </div>
 
-            <div className="w-[80%] h-[80%] absolute top-[10%] left-[10%] border border-blue-600 rounded-xl">
-                <div className="p-10 flex">
-                    <div className="flex-1 bg-white py-[21%]">xin chào</div>
-                    <div className="flex-1 bg-slate-400">xin chào 2</div>
+            <div className="w-[80%] h-[85%] absolute top-[10%] left-[10%] border border-blue-600 rounded-xl">
+                <div className="p-4 flex">
+                    <div className="flex-1 bg-white py-[23.5%]">xin chào</div>
+                    <div className="flex-[1.4] bg-slate-400">xin chào 2</div>
                 </div>
             </div>
         </div>
