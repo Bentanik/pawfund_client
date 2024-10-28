97ebc0fafbd028e838af260e97d13b3580fba34b
lamtanphat2202 <tphatt22@gmail.com>
2024-10-28 11:02:10 +0700

    fix get event and get events

--- src/app/(user)/event-detail/components/event-detail.tsx
+++ src/app/(user)/event-detail/components/event-detail.tsx
@@ -157,7 +157,7 @@ const EventDetail = ({ eventId }: EventDetail) => {
                 {/*Thumb hero */}
                 <div className="relative">
                     <img
-                        src="/images/home1.jpg"
+                        src={event?.eventDTO.thumbHeroUrl}
                         alt="thumb hero"
                         className="w-full h-[500px] object-cover "
                     />
@@ -224,7 +224,7 @@ const EventDetail = ({ eventId }: EventDetail) => {
                             <div className="flex ">
                                 <div className="flex-1 bg-slate-700 h-[500px]">
                                     <img
-                                        src="/images/time-event-bg.jpg"
+                                        src={event?.eventDTO.imagesUrl}
                                         alt="branch-bg"
                                         className="w-full h-full object-cover"
                                     />
